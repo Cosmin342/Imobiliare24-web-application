8988fa88218ac29fdfae8cd6489abb34ff86ffc1
Cosmin342 <cosminr47@gmail.com>
2023-04-21 00:50:14 +0300

    Added generated files from swagger

--- client/src/infrastructure/apis/client/apis/index.ts
+++ client/src/infrastructure/apis/client/apis/index.ts
@@ -1,5 +1,9 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './AddressApi';
+export * from './AnnouncementApi';
 export * from './AuthorizationApi';
+export * from './BuildingApi';
+export * from './NotificationApi';
 export * from './UserApi';
 export * from './UserFileApi';
